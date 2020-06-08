45e2df3e5d3a58506336e894260eda77a20fb32e
DaweiLiu0222 <DaweiLiu0222@users.noreply.github.com>
2020-06-07 21:35:44 -0500

    HW7

---
+++ HW7/HW7.X/build/default/production/SSD1306.o.d
@@ -0,0 +1,5 @@
+build/default/production/SSD1306.o:  \
+SSD1306.c  \
+ssd1306.h  \
+ i2c_master_noint.h  \
+font.h 
---
+++ HW7/HW7.X/build/default/production/main.o.d
@@ -0,0 +1,7 @@
+build/default/production/main.o:  \
+main.c  \
+adc.h  \
+ssd1306.h  \
+ i2c_master_noint.h  \
+font.h  \
+ws2812b.h 
